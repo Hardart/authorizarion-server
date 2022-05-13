dc4e228f35d1d74885c6c9609b8ef51537343e9e
Hardart <kaino@bk.ru>
2022-05-13 15:53:09 +0300

    small fixes

--- router/index.js
+++ router/index.js
@@ -6,17 +6,9 @@ const userController = require('../controllers/user-controller')
 const productsController = require('../controllers/products-controller')
 const { body } = require('express-validator')
 
-router.post('/registration', 
-	body('email').isEmail(), 
-	body('password').isLength({ min: 3, max: 32 }), 
-	userController.registration
-)
+router.post('/registration', body('email').isEmail(), body('password').isLength({ min: 3, max: 32 }), userController.registration)
 
-router.post('/login',
-	body('email').isEmail(), 
-	body('password').isLength({ min: 3, max: 32 }),  
-	userController.login
-)
+router.post('/login', body('email').isEmail(), body('password').isLength({ min: 3, max: 32 }), userController.login)
 router.delete('/logout', userController.logout)
 router.get('/refresh', userController.refresh)
 router.get('/users', authCheck, userController.getUsers)
--- server.js
+++ server.js
@@ -11,26 +11,26 @@ const errorMiddleware = require('./middlware/error-middleware')
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use(cookieParser())
-server.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+server.use(cors({ origin: 'http://localhost:3000' }))
 server.use('/api', router)
 server.use(errorMiddleware)
 
 async function start() {
-	try {
-		await mongoose.connect(
-			process.env.DB_CONN,
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			},
-			() => console.log('БД подключена\n===================================='),
-		)
-		server.listen(PORT, () => {
-			console.log(`====================================\nСервер запущен, порт: ${PORT}\n====================================`)
-		})
-	} catch (e) {
-		// console.log(e)
-	}
+   try {
+      await mongoose.connect(
+         process.env.DB_CONN,
+         {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+         },
+         () => console.log('БД подключена\n===================================='),
+      )
+      server.listen(PORT, () => {
+         console.log(`====================================\nСервер запущен, порт: ${PORT}\n====================================`)
+      })
+   } catch (e) {
+      // console.log(e)
+   }
 }
 
 start()
